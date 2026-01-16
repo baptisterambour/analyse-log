a1de4d54164cd24674bbdb318b81bd555a4e9829
pbertrand3409 <pierre.bertrand3409@gmail.com>
2026-01-16 17:36:57 +0100

    Update and rename lecteur.h to reader.h

---
+++ reader.h
@@ -0,0 +1,16 @@
+#ifndef READER_H
+#define READER_H
+
+#include <string>
+#include "graph.h"
+#include "config.h"
+
+class Reader {
+public:
+    Reader(const std::string& filename);
+
+private:
+
+};
+
+#endif // READER_H
